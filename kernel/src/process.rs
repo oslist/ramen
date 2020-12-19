511f599d11826db843a9d187a0c094e48b49d36d
toku-sa-n <tokusan441@gmail.com>
2020-12-19 23:42:47 +0900

    chore: define `Process` struct

--- kernel/src/process.rs
+++ kernel/src/process.rs
@@ -1,1 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use crate::mem::{allocator::page_box::PageBox, paging::pml4::PML4};
+use x86_64::structures::paging::{PageTable, PageTableFlags};
+
+struct Process {
+    pml4: PageBox<PageTable>,
+}
+impl Process {
+    fn new() -> Self {
+        Self {
+            pml4: PageBox::new(PageTable::new()),
+        }
+    }
+
+    fn init(&mut self) {
+        self.enable_recursive_mapping();
+        self.map_kernel_entry();
+    }
+
+    fn enable_recursive_mapping(&mut self) {
+        let a = self.pml4.phys_addr();
+        let f =
+            PageTableFlags::PRESENT | PageTableFlags::WRITABLE | PageTableFlags::USER_ACCESSIBLE;
+        self.pml4[511].set_addr(a, f)
+    }
+
+    fn map_kernel_entry(&mut self) {
+        let a = PML4.lock().level_4_table()[510].addr();
+        let f = PML4.lock().level_4_table()[510].flags();
+        self.pml4[510].set_addr(a, f);
+    }
+}
