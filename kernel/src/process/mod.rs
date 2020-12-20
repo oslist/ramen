3b6277747031d3ae33255d5a6d92498c66c19a67
toku-sa-n <tokusan441@gmail.com>
2020-12-20 10:29:33 +0900

    chore: push the initial values of the registers.

--- kernel/src/process/mod.rs
+++ kernel/src/process/mod.rs
@@ -2,22 +2,41 @@
 
 mod manager;
 
+use core::{convert::TryInto, mem, ptr};
+
 use crate::mem::{allocator::page_box::PageBox, paging::pml4::PML4};
-use x86_64::structures::paging::{PageTable, PageTableFlags};
+use os_units::Bytes;
+use x86_64::{
+    structures::paging::{PageSize, PageTable, PageTableFlags, Size4KiB},
+    VirtAddr,
+};
 
 pub struct Process {
     pml4: PageBox<PageTable>,
+    stack: PageBox<[u8]>,
+    rsp: VirtAddr,
+    rip: VirtAddr,
+    f: fn(),
 }
 impl Process {
-    fn new() -> Self {
+    fn new(f: fn()) -> Self {
+        let stack_size = Bytes::new(Size4KiB::SIZE.try_into().unwrap());
+        let stack = PageBox::new_slice(0, stack_size.as_usize());
+        let rsp = stack.virt_addr() + stack_size.as_usize() - mem::size_of::<InitialStack>();
+
         Self {
             pml4: PageBox::new(PageTable::new()),
+            stack,
+            rsp,
+            rip: VirtAddr::new((exec as usize).try_into().unwrap()),
+            f,
         }
     }
 
     fn init(&mut self) {
         self.enable_recursive_mapping();
         self.map_kernel_entry();
+        self.push_initial_register_values();
     }
 
     fn enable_recursive_mapping(&mut self) {
@@ -31,5 +50,58 @@ impl Process {
         let a = PML4.lock().level_4_table()[510].addr();
         let f = PML4.lock().level_4_table()[510].flags();
         self.pml4[510].set_addr(a, f);
+    }
+
+    fn push_initial_register_values(&mut self) {
+        let s = self.initial_stack();
+
+        // Safety: This operation is safe as the pointer is valid and is aligned.
+        unsafe { ptr::write(self.rsp.as_mut_ptr(), s) }
+    }
+
+    fn initial_stack(&self) -> InitialStack {
+        let self_addr = VirtAddr::new(self as *const Self as u64);
+        InitialStack::new(self_addr, self.rip)
+    }
+}
+
+/// Safety: `proc` must be a valid pointer.
+unsafe fn exec(p: *const Process) {
+    let p = &*p;
+    (p.f)();
+}
+
+struct CallerSavedRegisters {
+    rax: u64,
+    rcx: u64,
+    rdx: u64,
+    rsi: u64,
+    rdi: u64,
+    r8: u64,
+    r9: u64,
+    r10: u64,
+    r11: u64,
+}
+
+struct InitialStack {
+    regs: CallerSavedRegisters,
+    rip: VirtAddr,
+}
+impl InitialStack {
+    fn new(process: VirtAddr, rip: VirtAddr) -> Self {
+        Self {
+            regs: CallerSavedRegisters {
+                rax: 0,
+                rcx: 0,
+                rdx: 0,
+                rsi: 0,
+                rdi: process.as_u64(),
+                r8: 0,
+                r9: 0,
+                r10: 0,
+                r11: 0,
+            },
+            rip,
+        }
     }
 }
