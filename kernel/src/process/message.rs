e9d822ae422b4f3cba0b2846c1bf9e2ea9085d1e
toku-sa-n <tokusan441@gmail.com>
2020-12-26 19:17:24 +0900

    chore: define `message::SystemTask`

--- kernel/src/process/message.rs
+++ kernel/src/process/message.rs
@@ -1,1 +1,23 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+use os_units::{Bytes, NumOfPages};
+use x86_64::{
+    instructions::port::{PortReadOnly, PortWriteOnly},
+    structures::paging::Size4KiB,
+    PhysAddr, VirtAddr,
+};
+
+pub enum SystemTask {
+    Inb(PortReadOnly<u8>),
+    Outb(PortWriteOnly<u8>, u8),
+    Inl(PortReadOnly<u32>),
+    Outl(PortWriteOnly<u32>, u32),
+    Halt,
+    DisableInterrupt,
+    EnableInterrupt,
+    EnableInterruptAndHalt,
+    AllocatePages(NumOfPages<Size4KiB>),
+    DeallocatePages(VirtAddr, NumOfPages<Size4KiB>),
+    MapPages(PhysAddr, Bytes),
+    UnmapPages(VirtAddr, Bytes),
+}
