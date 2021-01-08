4a17f27656683165538a73a39204cd2601950807
toku-sa-n <tokusan441@gmail.com>
2021-01-08 23:27:30 +0900

    chore: define `new`s

--- kernel/src/process/message.rs
+++ kernel/src/process/message.rs
@@ -4,13 +4,28 @@ struct Message {
     header: Header,
     body: Body,
 }
+impl Message {
+    fn new(header: Header, body: Body) -> Self {
+        Self { header, body }
+    }
+}
 
 struct Header {
     sender: super::Id,
+}
+impl Header {
+    fn new(sender: super::Id) -> Self {
+        Self { sender }
+    }
 }
 
 struct Body {
     m1: u64,
     m2: u64,
     m3: u64,
+}
+impl Body {
+    fn new(m1: u64, m2: u64, m3: u64) -> Self {
+        Self { m1, m2, m3 }
+    }
 }
