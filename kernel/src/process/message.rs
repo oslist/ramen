6e14653f8b47aa4b099fda9ee70ea519d1a713f1
toku-sa-n <tokusan441@gmail.com>
2021-01-08 23:27:00 +0900

    chore: define structs

--- kernel/src/process/message.rs
+++ kernel/src/process/message.rs
@@ -1,1 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
+
+struct Message {
+    header: Header,
+    body: Body,
+}
+
+struct Header {
+    sender: super::Id,
+}
+
+struct Body {
+    m1: u64,
+    m2: u64,
+    m3: u64,
+}
